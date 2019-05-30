c47fa0c26f803e0939d662fdb0a89455b44617c7
suwit <ssupawas@gmail.com>
2019-05-30 13:22:50 +0700

    Player Url.mp3

---
+++ SuwitService/MyServiceViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MyServiceViewController.swift
+//  SuwitService
+//
+//  Created by suwit supawas on 30/5/2562 BE.
+//  Copyright © 2562 suwit supawas. All rights reserved.
+//
+
+import UIKit
+import AVFoundation // เป็น  class libraly ภายนอกเข้ามาใช้งาน
+
+class MyServiceViewController: UIViewController {
+    
+    var myPlayer: AVPlayer?  // ประกาศตัวแปรที่ใช้ class นี้เท่านั้น global
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    } // Main Method
+    
+
+    @IBAction func stopButton(_ sender: UIButton) {
+        
+        myPlayer?.pause()
+        
+    }
+    
+    
+    @IBAction func playButton(_ sender: UIButton) {
+        
+        let urlMP3: String = "https://febcthailand.com/m/mp3/answer/r.mp3"
+        let url = URL.init(string: urlMP3)
+        
+        
+        
+        let playerItem: AVPlayerItem = AVPlayerItem(url: url!)
+        myPlayer = AVPlayer(playerItem: playerItem)
+        let playerLayer: AVPlayerLayer = AVPlayerLayer(player: myPlayer)
+        
+        playerLayer.frame = CGRect(x: 0, y: 0, width: 10, height: 50)
+        self.view.layer.addSublayer(playerLayer)
+        myPlayer!.play() // ได้ปุ่มเปิดแล้วครับ
+        
+
+        
+    } // PlayButton
+    
+
+} // Main Class
--- SuwitService/ViewController.swift
+++ SuwitService/ViewController.swift
@@ -9,14 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }   // Main Method
     
     
+    @IBAction func signInButton(_ sender: UIButton) {
+        
+//        Get Value From TextField
+        let user: String = userTextField.text!
+        let password: String = passwordTextField.text!
+        
+        if (user.count == 0) || (password.count == 0)  {
+            myAlert(title: "มีช่องว่างนะจ๊ะ !!!", message: "กรุณากรอกให้ครบนะจ๊ะ") // ต้อง call methon หรือ function alert ที่เราสร้าง
+        } else {
+            
+            
+            
+        }
+        
+        
+        
+        
+    } // SignInButton
     
+    
+    
+    func checkAuthen(user: String, password: String) -> Void {
+        
+        
+        
+        
+    } // CheckAuthen
+    
+    // ต่อไปนี้เป็นการสร้าง function หรือ method  เพื่อให้มี popup ฟ้องขึ้นมาเมื่อกด signIn โดยที่ยังไม่ใส่ข้อมูล
+    
+    func myAlert(title: String, message: String) -> Void {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert) //object
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+            
+            alert.dismiss(animated: true, completion: nil)
+            
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+        
+        
+        
+        
+        
+    }
     
     
     @IBAction func signUpButton(_ sender: UIButton) {
